ded35a9e9d6cb552d101d61b5526ce33bd63dde5
tamusho329 <show_showed_shown0329@keio.jp>
2021-06-07 11:28:17 +0900

    6/7 ハンバーガーメニュー修正など

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// ハンバーガーメニュー
+
+$(function() {
+  $('.burger-btn').on('click', function() {
+    $('.header-nav').fadeToggle(300);
+    $(this).toggleClass('cross');
+  });
+
+  $('.header-item').on('click', function() {
+    $('.header-nav').fadeOut(300);
+    $('.burger-btn').removeClass('cross');
+  });
+});
